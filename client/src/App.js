762404865d3c745a0a2b2f67f639f6655fa6d5a1
SaaranshMishra <mishrasaaransh@gmail.com>
2019-07-21 23:47:59 +0530

    Updated client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          <h1>People Fetcher using React and NodeJS</h1>
+          <h1>People Fetcher</h1>
           <input
             style = {{
               fontSize: '2rem'
